83e4fc00a57864eb3537566b00e901831ba97c9f
michaeldouglassoares <michaeldouglas.10.94@gmail.com>
2022-04-23 19:22:39 -0300

    add pubsub

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,13 +5,28 @@ namespace App\Http\Controllers;
 use App\Models\Users;
 use Illuminate\Http\Request;
 
+use Google\Cloud\PubSub\PubSubClient;
+
+
 class UserController extends Controller
 {
     private $user;
+    private $gCloud;
 
     public function __construct(Users $user)
     {
         $this->user = $user;
+
+        try {
+            $pubsub = new PubSubClient([
+                'projectId' => env('APP_PROJECT_ID')
+            ]);
+        } catch (\Exception $e) {
+            die('aqui');
+        }
+        
+
+        $this->gCloud = $pubsub;
     }
 
     public function index()
@@ -25,7 +40,7 @@ class UserController extends Controller
         $age = $request->input('age');
         $email = $request->input('email');
 
-        if(empty($name) || empty($age) || empty($email)){
+        if (empty($name) || empty($age) || empty($email)) {
             return response()->json([
                 'message' => 'All fields is required'
             ], 400);
@@ -42,10 +57,10 @@ class UserController extends Controller
     {
         $user = $this->user::find($id);
 
-        if($user){
+        if ($user) {
             return response()->json($user);
         }
-        
+
         return response()->json([
             'message' => 'User not found'
         ], 400);
